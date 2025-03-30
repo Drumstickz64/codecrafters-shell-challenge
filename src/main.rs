764a61da6fe4a371019335d19a3d6367444a9194
Drumstickz64 <gearman533@gmail.com>
2025-03-30 23:53:32 +0300

    stage 4: attempt 1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-use std::io::{self, Write};
+use std::{
+    io::{self, Write},
+    process::ExitCode,
+};
 
-fn main() {
+use anyhow::{Context, Result};
+
+fn main() -> Result<ExitCode> {
     let prompt = "$ ";
 
     loop {
@@ -11,11 +16,43 @@ fn main() {
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
 
-        let output = evaluate(input.trim());
-        println!("{output}");
+        if input.is_empty() {
+            continue;
+        }
+
+        let cmd = parse(&input)?;
+
+        match cmd.program.as_str() {
+            "exit" => {
+                let Some(exit_code) = cmd.args.first() else {
+                    return Ok(ExitCode::SUCCESS);
+                };
+
+                let exit_code = exit_code.parse::<u8>().context(
+                    "unable to parse exit code, it must be a number in the range [0,255]",
+                )?;
+
+                return Ok(exit_code.into());
+            }
+            program => println!("{program}: command not found"),
+        }
     }
 }
 
-fn evaluate(cmd: &str) -> String {
-    format!("{cmd}: command not found")
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Cmd {
+    program: String,
+    args: Vec<String>,
+}
+
+fn parse(input: &str) -> Result<Cmd> {
+    let mut components = input.split_whitespace();
+    let program = components
+        .next()
+        .expect("expected input to not be empty")
+        .to_owned();
+
+    let args = components.map(|s| s.to_owned()).collect();
+
+    Ok(Cmd { program, args })
 }
