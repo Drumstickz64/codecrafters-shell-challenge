3323acbae857879efc5e61130d113c1adac7b949
Drumstickz64 <gearman533@gmail.com>
2025-03-31 22:58:59 +0300

    stage 9: attempt 1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::{
+    collections::HashMap,
     env::{self, consts::EXE_SUFFIX},
     fs,
     io::{self, Write},
     path::PathBuf,
     process::{Command, ExitCode},
+    sync::LazyLock,
 };
 
 use anyhow::{Context, Result};
@@ -14,6 +16,18 @@ const SYSTEM_PATH_SPERATOR: &str = ";";
 #[cfg(not(windows))]
 const SYSTEM_PATH_SPERATOR: &str = ":";
 
+type BuiltinFn = fn(Vec<String>) -> Result<Option<ExitCode>>;
+
+static SYSTEM_PATH: LazyLock<String> = LazyLock::new(|| env::var("PATH").unwrap_or_default());
+static BUILTINS: LazyLock<HashMap<&str, BuiltinFn>> = LazyLock::new(|| {
+    HashMap::from([
+        ("exit", builtin_exit as BuiltinFn),
+        ("echo", builtin_echo as BuiltinFn),
+        ("type", builtin_type as BuiltinFn),
+        ("pwd", builtin_pwd as BuiltinFn),
+    ])
+});
+
 fn main() -> Result<ExitCode> {
     let prompt = "$ ";
 
@@ -21,8 +35,7 @@ fn main() -> Result<ExitCode> {
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .init();
 
-    let system_path = env::var("PATH")?;
-    debug!(system_path);
+    debug!(?SYSTEM_PATH);
 
     loop {
         print!("{prompt}");
@@ -40,46 +53,20 @@ fn main() -> Result<ExitCode> {
         let cmd = parse(&input)?;
         debug!(?cmd);
 
-        match cmd.program.as_str() {
-            "exit" => {
-                let Some(exit_code) = cmd.args.first() else {
-                    return Ok(ExitCode::SUCCESS);
-                };
+        let program = cmd.program.as_str();
+        if let Some(builtin_fn) = BUILTINS.get(program) {
+            let exit_code = builtin_fn(cmd.args)?;
+            if let Some(exit_code) = exit_code {
+                return Ok(exit_code);
+            }
+        } else if find_executable(&SYSTEM_PATH, program).is_some() {
+            debug!(program, "executing program");
+            let output = Command::new(program).args(cmd.args).output().unwrap();
 
-                let exit_code = exit_code.parse::<u8>().context(
-                    "unable to parse exit code, it must be a number in the range [0,255]",
-                )?;
-
-                return Ok(exit_code.into());
-            }
-            "echo" => {
-                debug!("executable builtin command 'echo'");
-                let output = cmd.args.join(" ");
-                println!("{output}");
-            }
-            "type" => {
-                debug!("executable builtin command 'type'");
-                for arg in cmd.args {
-                    if ["exit", "echo", "type"].contains(&arg.as_str()) {
-                        println!("{arg} is a shell builtin")
-                    } else if let Some(executable_path) = find_executable(&system_path, &arg) {
-                        println!("{arg} is {}", executable_path.display());
-                    } else {
-                        println!("{arg}: not found");
-                    }
-                }
-            }
-            program => {
-                if find_executable(&system_path, program).is_some() {
-                    debug!(program, "executing program");
-                    let output = Command::new(program).args(cmd.args).output().unwrap();
-
-                    io::stdout().write_all(&output.stdout).unwrap();
-                    io::stderr().write_all(&output.stderr).unwrap();
-                } else {
-                    println!("{program}: command not found");
-                }
-            }
+            io::stdout().write_all(&output.stdout).unwrap();
+            io::stderr().write_all(&output.stderr).unwrap();
+        } else {
+            println!("{program}: command not found");
         }
     }
 }
@@ -130,4 +117,47 @@ fn find_executable(system_path: &str, executable_name: &str) -> Option<PathBuf> 
     }
 
     None
+}
+
+fn builtin_exit(args: Vec<String>) -> Result<Option<ExitCode>> {
+    let Some(exit_code) = args.first() else {
+        return Ok(Some(ExitCode::SUCCESS));
+    };
+
+    let exit_code = exit_code
+        .parse::<u8>()
+        .context("unable to parse exit code, it must be a number in the range [0,255]")?;
+
+    Ok(Some(exit_code.into()))
+}
+
+fn builtin_echo(args: Vec<String>) -> Result<Option<ExitCode>> {
+    debug!("executable builtin command 'echo'");
+    let output = args.join(" ");
+    println!("{output}");
+    Ok(None)
+}
+
+fn builtin_type(args: Vec<String>) -> Result<Option<ExitCode>> {
+    debug!("executable builtin command 'type'");
+
+    for arg in args {
+        if BUILTINS.contains_key(&arg.as_str()) {
+            println!("{arg} is a shell builtin")
+        } else if let Some(executable_path) = find_executable(&SYSTEM_PATH, &arg) {
+            println!("{arg} is {}", executable_path.display());
+        } else {
+            println!("{arg}: not found");
+        }
+    }
+
+    Ok(None)
+}
+
+fn builtin_pwd(_args: Vec<String>) -> Result<Option<ExitCode>> {
+    debug!("executable builtin command 'pwd'");
+
+    println!("{}", env::current_dir()?.display());
+
+    Ok(None)
 }
